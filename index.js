ba605570bc31424367e7e000e2d2ff262cdf1aa8
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-10-08 17:26:56 +0100

    server ready for testing of basic features

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ var async = require("async");
 
 
 /**** Final Variables & Templates ****/
-var ticket_queue_path = __dirname + "/data/ticket_queue.json"
+var ticket_queue_path = __dirname + "/data/ticket_queue.json";
 
-var ticket_template = '{"ticket_number":-1,"ticket_type":"A"}';
-var ticket_brief_template = '{"ticket_number":-1}';
-var error_template = '{"code":-1,"message":"A","fields":"xpto"}';
+var ticket_template = JSON.parse('{"ticket_number":-1,"request_timestamp":0}');
+var ticket_brief_template = JSON.parse('{"ticket_number":-1}');
+var error_template = JSON.parse('{"code":-1,"message":"A","fields":"xpto"}');
+var everyThing_template = JSON.parse('[{"type": "A","queue": [],"last_ticket": 0}]');
+var queue_template = JSON.parse('{"type": "xpto","queue": [],"last_ticket": 0}');
+
+
+var average_time = JSON.parse('[{"type":"A", "number_of_tickets":0, "average_time":0}]');
+var closed_for_requests = false;
 
 /**** Initial Things *****/
 
@@ -42,6 +48,41 @@ app.use(bodyParser.urlencoded({
 
 
 /**** Private functions ****/
+
+function findTicketInQueue(queue, ticket_number) {
+
+    console.log('findTicketInQueue - checking if ticket number is at the top of the queue');
+    if (ticket_number == queue[0]['ticket_number']) {
+        console.log('findTicketInQueue - ticket is at the top of the queue - everything goes as planned');
+        return 0;
+        //res.status(200).end("gold");
+    }
+    else {
+        console.log('findTicketInQueue - ticket is not at the top of the queue - check if bogus');
+        if (ticket_number < queue[0]['ticket_number']) {
+            console.log('findTicketInQueue - ticket is not in the queue');
+            return -1;
+        }
+        else {
+            for (var i = 1; i < queue.length; i++) {
+                if (ticket_number == queue[i]['ticket_number']) {
+                    console.log('findTicketInQueue - ticket found in pos %d', i);
+                    return i;
+                }
+            }
+        }
+        //res.status(200).end("shit");
+    }
+}
+
+function removeTicketFromQueue(queue, ticket_pos) {
+
+    var newQueue = queue;
+    newQueue.splice(0, ticket_pos + 1);
+
+    return newQueue;
+}
+
 
 /**** GET METHODS ****/
 
@@ -82,22 +123,37 @@ app.get('/employee/nextTicket', function (req, res) {
 
         if (ticket_type != null) {
 
-            var next_ticket = JSON.parse(ticket_brief_template);
+            var next_ticket = ticket_brief_template;
             console.log('employee nextTicket - searching ticket type (%s) queue', ticket_type);
+
+            var queue_pos = -1;
 
             for (var i = 0; i < queues.length; i++) {
                 if (queues[i]['type'] == ticket_type) {
                     console.log('employee nextTicket - found ticket type queue');
-                    next_ticket['ticket_number'] = queues[i]['queue'][0]['ticket_number'];
+                    queue_pos = i;
                     break;
                 }
             }
-            res.status(200).end(JSON.stringify(next_ticket));
+
+            if (queue_pos != -1 && queues[queue_pos]['queue'].length > 0) {
+                next_ticket['ticket_number'] = queues[queue_pos]['queue'][0]['ticket_number'];
+                res.status(200).end(JSON.stringify(next_ticket));
+            }
+            else {
+                console.log('employee nextTicket - bad request (no more tickets or wrong type)');
+                var error_resp = error_template;
+                error_resp['code'] = 410;
+                error_resp['message'] = "Gone - no more tickets or type non-existent";
+                error_resp['fields'] = "none";
+                console.log(error_resp);
+                res.status(400).end(JSON.stringify(error_resp));
+            }
 
         }
         else {
             console.log('employee nextTicket - bad request (no ticket type parameter)');
-            var error_resp = JSON.parse(error_template);
+            var error_resp = error_template;
             error_resp['code'] = 400;
             error_resp['message'] = "bad request";
             error_resp['fields'] = "ticket_type";
@@ -106,7 +162,6 @@ app.get('/employee/nextTicket', function (req, res) {
 
         }
     });
-
 
 });
 
@@ -130,7 +185,7 @@ app.get('/employee/fullQueue', function (req, res) {
             for (var i = 0; i < queues.length; i++) {
                 if (queues[i]['type'] == ticket_type) {
                     console.log('employee fullQueue - found ticket type queue');
-                    var ticket_queue= queues[i]['queue'];
+                    var ticket_queue = queues[i]['queue'];
                     break;
                 }
             }
@@ -139,7 +194,7 @@ app.get('/employee/fullQueue', function (req, res) {
         }
         else {
             console.log('employee fullQueue - bad request (no ticket type parameter)');
-            var error_resp = JSON.parse(error_template);
+            var error_resp = error_template;
             error_resp['code'] = 400;
             error_resp['message'] = "bad request";
             error_resp['fields'] = "ticket_type";
@@ -157,7 +212,6 @@ app.get('/employee/fullQueue', function (req, res) {
 app.get('/client/remainingTime', function (req, res) {
     console.log('client remainingTime - entered');
     res.status(200).end("client/remainingTime...");
-
 });
 
 
@@ -167,19 +221,181 @@ app.get('/client/remainingTime', function (req, res) {
 
 app.post('/employee/ticketAttended', function (req, res) {
     console.log('employee ticketAttended - entered');
-    res.status(200).end("employee/ticketAttended...");
+    var ticket_req = req.body.ticket;
+    var filesPath = [ticket_queue_path];
+
+    //console.log(JSON.stringify(ticket_req));
+
+    async.map(filesPath, function (filePath, cb) { //reading files or dir
+        fs.readFile(filePath, 'utf8', cb);
+    }, function (err, results) {
+        //console.log(data[0]['type'])
+        var queues = JSON.parse(results[0]);
+
+        if (ticket_req.ticket_type != null && ticket_req.ticket_number != null && ticket_req.ticket_number > 0) {
+
+            console.log('employee ticketAttended - searching ticket type (%s) queue', ticket_req.ticket_type);
+
+            for (var i = 0; i < queues.length; i++) {
+                if (queues[i]['type'] == ticket_req.ticket_type) {
+                    console.log('employee ticketAttended - found ticket type queue');
+                    var ticket_pos = findTicketInQueue(queues[i]['queue'], ticket_req.ticket_number);
+                    var queue_pos = i;
+                    break;
+                }
+            }
+
+            if (ticket_pos != -1) {
+                //update average time (TO DO)
+                //updateAverageTime(queues[queue_pos]['type'], ticket_pos+1, new Date());
+
+
+                //console.log(queues[queue_pos]);
+                queues[queue_pos]['queue'] = removeTicketFromQueue(queues[queue_pos]['queue'], ticket_pos);
+                //console.log(queues[queue_pos]);
+                console.log('employee ticketAttended - writing new queue');
+
+                fs.writeFile(ticket_queue_path, JSON.stringify(queues), function (err) {
+                    console.error(err)
+                });
+
+                var result = '{"result":"success"}';
+                res.status(200).end(result);
+
+            }
+            else {
+                console.log('employee ticketAttended - bogus ticket number');
+                var error_resp = error_template;
+                error_resp['code'] = 400;
+                error_resp['message'] = "bad request - wrong ticket number or type";
+                error_resp['fields'] = "ticket_type or ticket_number";
+                console.log(error_resp);
+                res.status(400).end(JSON.stringify(error_resp));
+            }
+
+
+        }
+        else {
+            console.log('employee ticketAttended - bad request (no ticket type or ticket number parameter)');
+            var error_resp = error_template;
+            error_resp['code'] = 400;
+            error_resp['message'] = "bad request - missing ticket number or type";
+            error_resp['fields'] = "ticket_type or ticket_number";
+            console.log(error_resp);
+            res.status(400).end(JSON.stringify(error_resp));
+
+        }
+
+    });
+    //res.status(200).end("employee/ticketAttended...");
 
 });
 
 app.post('/employee/closeForTheDay', function (req, res) {
     console.log('employee closeForTheDay - entered');
-    res.status(200).end("employee/closeForTheDay...");
+
+    var result = '{"result":"success"}';
+    closed_for_requests = true;
+
+    res.status(200).end(result);
 
 });
 
 app.post('/employee/newDay', function (req, res) {
     console.log('employee newDay - entered');
-    res.status(200).end("employee/newDay...");
+
+    var result = '{"result":"success"}';
+
+    fs.writeFile(ticket_queue_path, JSON.stringify(everyThing_template), function (err) {
+        console.error(err)
+    });
+
+    closed_for_requests = false;
+
+
+    res.status(200).end(result);
+
+});
+
+app.post('/employee/makeNewType', function (req, res) {
+    console.log('employee makeNewType - entered');
+    var ticket_req = req.body.ticket_type;
+    var filesPath = [ticket_queue_path];
+
+    //console.log(JSON.stringify(ticket_req));
+
+    if (closed_for_requests == false) {
+        async.map(filesPath, function (filePath, cb) { //reading files or dir
+            fs.readFile(filePath, 'utf8', cb);
+        }, function (err, results) {
+            //console.log(data[0]['type'])
+            var queues = JSON.parse(results[0]);
+
+            if (ticket_req.ticket_type != null) {
+
+                console.log('employee makeNewType - searching ticket type (%s) queue', ticket_req.ticket_type);
+
+                queue_pos = -1;
+
+                for (var i = 0; i < queues.length; i++) {
+                    if (queues[i]['type'] == ticket_req.ticket_type) {
+                        console.log('employee makeNewType - found ticket type queue');
+                        var queue_pos = i;
+                        break;
+                    }
+                }
+
+                if (queue_pos == -1) {
+
+                    console.log('employee makeNewType - writing new queue');
+
+                    var new_queue = queue_template;
+                    new_queue['type'] = ticket_req.ticket_type;
+                    queues[queues.length] = new_queue;
+
+                    fs.writeFile(ticket_queue_path, JSON.stringify(queues), function (err) {
+                        console.error(err)
+                    });
+
+                    var result = '{"result":"success"}';
+                    res.status(200).end(result);
+
+                }
+                else {
+                    console.log('employee makeNewType - type already exists');
+                    var error_resp = error_template;
+                    error_resp['code'] = 400;
+                    error_resp['message'] = "bad request - type already exists";
+                    error_resp['fields'] = "ticket_type";
+                    console.log(error_resp);
+                    res.status(400).end(JSON.stringify(error_resp));
+                }
+
+
+            }
+            else {
+                console.log('employee makeNewType - bad request (no ticket type parameter)');
+                var error_resp = error_template;
+                error_resp['code'] = 400;
+                error_resp['message'] = "bad request - missing ticket type";
+                error_resp['fields'] = "ticket_type";
+                console.log(error_resp);
+                res.status(400).end(JSON.stringify(error_resp));
+
+            }
+
+        });
+    }
+    else {
+        console.log('employee makeNewType - too late t request ticket');
+        var error_resp = error_template;
+        error_resp['code'] = 400;
+        error_resp['message'] = "Gone - service terminated for the day";
+        error_resp['fields'] = "none";
+        console.log(error_resp);
+        res.status(410).end(JSON.stringify(error_resp));
+    }
+    //res.status(200).end("employee/ticketAttended...");
 
 });
 
@@ -188,14 +404,88 @@ app.post('/employee/newDay', function (req, res) {
 
 app.post('/client/requestTicket', function (req, res) {
     console.log('client requestTicket - entered');
-    res.status(200).end("client/requestTicket...");
+
+    if (closed_for_requests == false) {
+        var ticket_req = req.body.ticket_request;
+        var filesPath = [ticket_queue_path];
+
+        //console.log(JSON.stringify(ticket_req));
+
+        async.map(filesPath, function (filePath, cb) { //reading files or dir
+            fs.readFile(filePath, 'utf8', cb);
+        }, function (err, results) {
+            //console.log(data[0]['type'])
+            var queues = JSON.parse(results[0]);
+
+            if (ticket_req.ticket_type != null) {
+
+                console.log('client requestTicket - searching ticket type (%s) queue', ticket_req.ticket_type);
+
+                var queue_pos = -1;
+
+                for (var i = 0; i < queues.length; i++) {
+                    if (queues[i]['type'] == ticket_req.ticket_type) {
+                        console.log('client requestTicket - found ticket type queue');
+                        queue_pos = i;
+                        break;
+                    }
+                }
+                if (queue_pos != -1) {
+                    console.log(queues[queue_pos]);
+                    var new_ticket = ticket_template;
+                    new_ticket['ticket_number'] = queues[queue_pos]['last_ticket'] + 1;
+                    new_ticket['request_timestamp'] = new Date().getTime();
+                    queues[queue_pos]['queue'][queues[queue_pos]['queue'].length] = new_ticket;
+                    queues[queue_pos]['last_ticket'] = new_ticket['ticket_number'];
+
+                    console.log(queues[queue_pos]);
+                    fs.writeFile(ticket_queue_path, JSON.stringify(queues), function (err) {
+                        console.error(err)
+                    });
+
+                    var result = '{"result":"success"}';
+                    res.status(200).end(result);
+                }
+                else {
+                    console.log('client requestTicket - bad request (ticket type non-existent)');
+                    var error_resp = error_template;
+                    error_resp['code'] = 400;
+                    error_resp['message'] = "bad request - ticket type non-existent";
+                    error_resp['fields'] = "ticket_type";
+                    console.log(error_resp);
+                    res.status(400).end(JSON.stringify(error_resp));
+                }
+
+            }
+            else {
+                console.log('client requestTicket - bad request (no ticket type parameter)');
+                var error_resp = error_template;
+                error_resp['code'] = 400;
+                error_resp['message'] = "bad request - missing ticket type";
+                error_resp['fields'] = "ticket_type";
+                console.log(error_resp);
+                res.status(400).end(JSON.stringify(error_resp));
+
+            }
+
+        });
+    }
+    else {
+        console.log('client requestTicket - too late t request ticket');
+        var error_resp = error_template;
+        error_resp['code'] = 400;
+        error_resp['message'] = "Gone - service terminated for the day";
+        error_resp['fields'] = "none";
+        console.log(error_resp);
+        res.status(410).end(JSON.stringify(error_resp));
+    }
 
 });
 
 
 /**** Put server running ****/
 
-var server = app.listen(80, function () {
+var server = app.listen(5000, function () {
 
     var host = server.address().address;
     var port = server.address().port;
