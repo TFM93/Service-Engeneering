a4b9f689dd909b6bae07cecadd416fbf0a60a707
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-10-20 16:19:32 +0000

    ..

--- nfc_endpoint.py
+++ nfc_endpoint.py
@@ -1,19 +1,48 @@
 import nfc
+import nfc.ndef
 import requests
 
 
 def connected(tag):
     print('tag has been connected')
     print(tag)
-    print('calling server')
+
+    tag_id = str(tag).split(' ')[4].split('=')[1]
+    print("tag id: " + tag_id)
+    #print("tag message: " + tag.ndef.message)
+"""
+    new_ndef = nfc.ndef
+
+    record1 = nfc.ndef.Record("urn:nfc:wkt:T", "id1", "\x02enHello World!")
+    record2 = nfc.ndef.Record("urn:nfc:wkt:T", "id2", "\x02deHallo Welt!")
+    message = nfc.ndef.Message(record1, record2)
+    print(message.pretty())
+
+    new_ndef.message = message
+
+
+    print(str(message))
+
+    f = open('abc.txt', 'w')
+    f.write(str(message)
+    f.close()
+
+    #print("is writable: " + tag.ndef.is_writable)
+
+    print(tag.ndef.message.pretty() if tag.ndef else "Sorry, no NDEF")
+
+    #tag.ndef = new_ndef
+
+    print(tag.ndef.message.pretty() if tag.ndef else "Sorry, no NDEF")
+"""
 
     ticket_type = 'A'
+#    ticket_type = raw_input('choose ticket type: ')
+    
+    print('calling server')
 
-    ticket_type = raw_input('choose ticket type: ')
-
-    body = {"ticket_request":{"ticket_type":ticket_type}}
-
-    resp = requests.post('http://192.168.1.78:80/client/requestTicket', json=body)
+    body = {"ticket_request":{"ticket_type":ticket_type}, "endpoint_id": tag_id}
+    resp = requests.post('http://localhost/client/requestTicket', json=body)
 
     if resp.status_code == 200:
         print('done')
@@ -26,6 +55,7 @@ def connected(tag):
         print('this is bad')
 
     return False
+
 
 clf = nfc.ContactlessFrontend('usb')
 clf.connect(rdwr={'on-connect': connected})
