ee7f014cdf3fa3adfb31b91b6f5e82ee9e757399
danifss <daniel.silva@ua.pt>
2016-11-08 13:37:00 +0000

    Changed index and requirements. API restructured

--- authservice/api/urls.py
+++ authservice/api/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
     # ex: /api/authentication/users/
     url(r'^authentication/users/$', views.ListUsers.as_view()),
 
-    # ex: /api/authentication/login/
-    url(r'^authentication/login/$', views.Login.as_view()),
-    # ex: /api/authentication/logout/1
-    url(r'^authentication/logout/(?P<pk>[0-9]+)$', views.Logout.as_view()),
+    # ex: /api/authentication/login/facebook/
+    url(r'^authentication/login/(?P<provider>[a-zA-Z0-9]+)/$', views.Login.as_view()),
+    # ex: /api/authentication/isLogged/1/
+    url(r'^authentication/isLogged/(?P<pk>[0-9]+)/$', views.IsLogged.as_view()),
+    # ex: /api/authentication/logout/1/
+    url(r'^authentication/logout/(?P<pk>[0-9]+)/$', views.Logout.as_view()),
+    # ex: /api/authentication/delete/1/
+    url(r'^authentication/delete/(?P<pk>[0-9]+)/$', views.DeleteUser.as_view()),
 
 ]
--- authservice/api/views.py
+++ authservice/api/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
+from django.http import HttpResponseRedirect
 from allauth.account.models import EmailAddress
 from serializers import EmailAddressSerializer
 
@@ -17,11 +18,11 @@ class ListUsers(generics.ListAPIView):
 
         <h3>Details</h3>
 
-        METHODS : GET
+        <b>METHODS:</b>
+            - GET
 
         <b>RETURNS:</b>
-
-        - 200 OK.
+            - 200 OK
 
         """
         # result = [users for users in EmailAddress.objects.all()]
@@ -29,14 +30,18 @@ class ListUsers(generics.ListAPIView):
 
 
 class Login(APIView):
-    """ Login User """
     allowed_methods = ['POST']
 
-    def post(self, request):
+    def post(self, request, provider=None):
         """
         Login user in the system.
 
-        <b>Details</b>
+        <h3>Details</h3>
+        <b>Providers available:</b>
+        - Facebook
+        - Twitter
+        - Google
+        - LinkedIn
 
         <b>METHODS:</b>
             - POST
@@ -48,36 +53,100 @@ class Login(APIView):
         # print request.META['HTTP_X_CSRFTOKEN']
         # X-CSRFToken: zhOXQAEtUqXoolDN66tlSJ76zKLPl48N
         # Content-Type: application/json
+        if provider is not None:
+            try:
+                # print provider
+                prov = provider.upper()
+                if prov == 'FACEBOOK':
+                    print 'Facebook login'
+                    return HttpResponseRedirect('http://localhost:8000')
+                    # return redirect('http://localhost:8000/accounts/facebook/login/', status.HTTP_200_OK)
+                if prov == 'TWITTER':
+                    print 'Twitter login'
+                    return Response(status=status.HTTP_200_OK)
+                if prov == 'GOOGLE':
+                    print 'Google login'
+                    return Response(status=status.HTTP_200_OK)
+                if prov == 'LINKEDIN':
+                    print 'LinkedIn login'
+                    return Response(status=status.HTTP_200_OK)
+
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class IsLogged(APIView):
+    allowed_methods = ['GET']
+
+    def get(self, request):
+        """
+        Checks if given user id is logged in the system
+
+        <h3>Details</h3>
+
+        <b>METHODS:</b>
+            - GET
+
+        <b>RETURNS:</b>
+            - 200 OK
+            - 404 NOT FOUND
+            - 400 BAD REQUEST
+        """
+        return Response(status=status.HTTP_200_OK)
+
+
+class Logout(APIView):
+    allowed_methods = ['POST']
+
+    def post(self, request, pk=None):
+        """
+        Logout user from the system.
+
+        <h3>Details</h3>
+
+        <b>METHODS:</b>
+            - POST
+
+        <b>RETURNS:</b>
+            - 200 OK
+            - 404 NOT FOUND
+            - 400 BAD REQUEST
+        """
+        # print request.META['HTTP_X_CSRFTOKEN']
+        # X-CSRFToken: zhOXQAEtUqXoolDN66tlSJ76zKLPl48N
+        # Content-Type: application/json
         if request is not None:
             return Response(status=status.HTTP_200_OK)
 
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-class Logout(generics.DestroyAPIView):
+class DeleteUser(APIView):
     # queryset = User.objects.all()
     # serializer_class = UserSerializer
     allowed_methods = ['DELETE']
 
     def delete(self, request, pk=None):
         """
-        Deletes a users logged by given user id
+        Deletes a users by given id
 
-        <b>Details</b>
+        <h3>Details</h3>
 
-        METHODS : DELETE
+        <b>METHODS:</b>
+            - DELETE
 
         <b>RETURNS:</b>
-        - 200 OK
-        - 404 NOT FOUND
-        - 400 BAD REQUEST
+            - 200 OK
+            - 404 NOT FOUND
+            - 400 BAD REQUEST
         """
         # X-CSRFToken: kN2QGs072FqY47lb6BTp1q1B9cPqlqDPuAVxMWYntlH3O3PpAD0kaLFpLejHGQVB
         # print(request.META['CSRF_COOKIE'])
         if pk is not None:
             try:
                 int_id = int(pk)
-                # user = User.objects.get(id=int_id)
+                user = EmailAddress.objects.get(id=int_id)
                 # user.delete()
                 return Response(status=status.HTTP_200_OK, data={"detail": "User deleted with success."})
             except:
