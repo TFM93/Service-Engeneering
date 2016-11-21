539464236f40fc72ec4a152fe0664e2515b0bfe5
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-11-21 09:49:14 +0000

    ticket dispenser - PC version done

--- nfc_endpoint.py
+++ nfc_endpoint.py
@@ -5,17 +5,22 @@ import requests
 
 current_ticket_type = 'A'
 current_tag = " "
+must_go_on = True
 
 def set_current_tag(tag):
     global current_tag
     current_tag = tag
+
+def set_must_go_on(must):
+    global must_go_on
+    must_go_on = must
 
 def get_current_tag():
     global current_tag
     return current_tag
 
 def connected(tag):
-
+    '''
     if current_tag == str(tag):
         print('please remove card')
         print('waiting')
@@ -23,9 +28,9 @@ def connected(tag):
 
         return False
         pass
-
-    print('tag has been connected')
-    print(tag)
+    '''
+    print('\ntag has been connected')
+    #print(tag)
 
     set_current_tag(str(tag))
 
@@ -41,34 +46,41 @@ def connected(tag):
     print("tag id: " + tag_id)
     #print("tag message: " + tag.ndef.message)
 
-    ticket_type = current_ticket_type
+    #ticket_type = current_ticket_type
+    ticket_type = raw_input('\nticket type?')
 #    ticket_type = raw_input('choose ticket type: ')
     
     print('calling server')
 
     body = {"ticket_request":{"ticket_type":ticket_type, "endpoint_id": tag_id}}
 
-    print(body)
+    #print(body)
 
     resp = requests.post('http://localhost/client/requestTicket', json=body)
 
     if resp.status_code == 200:
-        print('done')
-        print('your ticket number is:')
-
+        print('\ndone')
         result = resp.json()
+        print('your ticket number is:')
         print(result['ticket_number'])
+
+        if(result['code'] != 'ack' and result['code'] != 'no_ack'):
+            print('this card is not registered. Your code is:')
+            print(result['code'])        
 
     else:
         result = resp.json()
 
-        print('Error')
+        print('\nError')
         print('code: ' + str(result['code']) + ', message: ' + result['message'])
 
+        if(result['message'] == "Gone - service terminated for the day"):
+            set_must_go_on(False)
 
-    print('waiting')
+
+    #print('waiting')
     time.sleep(1)
-
+    print('\nwaiting for card')
 
     return tag
 
@@ -77,12 +89,12 @@ def connected(tag):
 print('\nwaiting for card')
 
 clf = nfc.ContactlessFrontend('usb')
-must_go_on = True
+
 
 while must_go_on == True:
     clf.connect(rdwr={'on-connect': connected})
-    print('current_tag: ' + current_tag)
-    must_go_on = raw_input('go on?') == '1'
+    #print('current_tag: ' + current_tag)
+    #must_go_on = raw_input('\ngo on?') == '1'
 
     pass
 
