8d4d6b664a71f67e045ca9e73da0c290583d2ba8
dani.fsec@gmail.com <daniel.asd.09>
2016-12-02 11:53:28 +0000

    Removed old files.

--- authservice/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from allauth.account.models import EmailAddress
-
-
-class EmailAddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EmailAddress
-        fields = '__all__'
-        # fields = ('id', 'user', 'email', 'verified', 'primary', 'objects')
