a411db2c1ee2aa2576033df0e0259370f5e48297
dani.fsec@gmail.com <daniel.asd.09>
2016-12-04 14:14:18 +0000

    Added phone number support for users.

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
 
     # ex: /api/authentication/user/delete/1/
     # url(r'^authentication/user/delete/(?P<pk>[0-9]+)/$', views.DeleteUser.as_view()),
+    # ex: /api/authentication/user/logout/
+    # url(r'^authentication/user/logout/$', views.LogoutUser.as_view()),
     # ex: /api/authentication/user/12F830DF/
     url(r'^authentication/user/uuid/(?P<uuid>[a-zA-Z0-9]+)/$', views.GetUserInfoByUUID.as_view()),
     # ex: /api/authentication/user/1/
